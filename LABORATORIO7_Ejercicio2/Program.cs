d111ee098c45bb061382e9a94738ee1e7446d142
PC <PC@DESKTOP-NOE8GP1>
2023-10-04 12:45:18 -0500

    Agregar archivos de proyecto.

---
+++ LABORATORIO7_Ejercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LABORATORIO7_Ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+
+            for(i = 1; i <= 1000; i++)
+            {
+                if(i % 3 == 0)
+                {
+                    int divisor = i / 3;
+                    Console.WriteLine("Divisor de Fiorella Romero " + divisor + " : " + i);
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
